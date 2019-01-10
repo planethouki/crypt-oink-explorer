5f3bc0c3d46ed49b6454b231401be10d109366b9
planethouki <26595148+planethouki@users.noreply.github.com>
2019-01-10 10:05:07 +0900

    ton

--- dist/js/ton.js
+++ dist/js/ton.js
@@ -12,44 +12,83 @@ let sammy;
         });
     });
 
-    async function getEntityFromTokenId(tokenId) {
+    async function getStateFromTokenId(tokenId) {
         const getEntity = new Promise((resolve, reject) => {
             contracts.EntityCore.getEntity(tokenId, {}, (error, result) => {
                 if (error) reject(error);
-                if (result) {
-                    resolve({
-                        "id": tokenId,
-                        "tokenId": tokenId,
-                        "isBreeding": result[0].toString(),
-                        "isReady": result[1].toString(),
-                        "cooldownIndex": result[2].toNumber(),
-                        "nextActionAt": result[3].toNumber(),
-                        "matingWithId": result[4].toNumber(),
-                        "birthTime": result[5].toNumber(),
-                        "breederId": result[6].toNumber(),
-                        "seederId": result[7].toNumber(),
-                        "generation": result[8].toNumber(),
-                        "dna": result[9].toString(16),
-                    });
-                }
-                resolve();
+                resolve(result);
             });
         });
         const ownerOf = new Promise((resolve, reject) => {
             contracts.EntityCore.ownerOf(tokenId, {}, (error, result) => {
-                if (error) console.log(error);
-                if (result) {
-                    resolve({
-                        "owner": result
-                    });
-                }
-                resolve();
+                if (error) reject(error);
+                resolve(result);
             });
         });
-        const entityAndOwner = await Promise.all([getEntity, ownerOf]);
-        let entity = entityAndOwner[0];
-        entity["owner"] = entityAndOwner[1]["owner"];
-        return entity;
+        const getAuctionSell = new Promise((resolve, reject) => {
+            contracts.AuctionSell.getAuction(tokenId, {}, (error, result) => {
+                if (error) reject(error);
+                resolve(result);
+            });
+        });
+        const getCurrentSellPrice = new Promise((resolve, reject) => {
+            contracts.AuctionSell.getCurrentPrice(tokenId, {}, (error, result) => {
+                if (error) reject(error);
+                resolve(result);
+            });
+        });
+        const getAuctionSeed = new Promise((resolve, reject) => {
+            contracts.AuctionSeed.getAuction(tokenId, {}, (error, result) => {
+                if (error) reject(error);
+                resolve(result);
+            });
+        });
+        const getCurrentSeedPrice = new Promise((resolve, reject) => {
+            contracts.AuctionSeed.getCurrentPrice(tokenId, {}, (error, result) => {
+                if (error) reject(error);
+                resolve(result);
+            });
+        });
+        const entityAndOwner = await Promise.all([getEntity, ownerOf, getAuctionSell, getCurrentSellPrice, getAuctionSeed, getCurrentSeedPrice]);
+        const resultEntity = entityAndOwner[0];
+        const resultOwner = entityAndOwner[1];
+        const resultAuctionSell = entityAndOwner[2];
+        const resultSellPrice = entityAndOwner[3];
+        const resultAuctionSeed = entityAndOwner[4];
+        const resultSeedPrice = entityAndOwner[5];
+        return {
+            entity: {
+                "id": tokenId,
+                "tokenId": tokenId,
+                "isBreeding": resultEntity[0].toString(),
+                "isReady": resultEntity[1].toString(),
+                "cooldownIndex": resultEntity[2].toNumber(),
+                "nextActionAt": resultEntity[3].toNumber(),
+                "matingWithId": resultEntity[4].toNumber(),
+                "birthTime": resultEntity[5].toNumber(),
+                "breederId": resultEntity[6].toNumber(),
+                "seederId": resultEntity[7].toNumber(),
+                "generation": resultEntity[8].toNumber(),
+                "owner": resultOwner,
+                "dna": resultEntity[9].toString(16),
+            },
+            shop: {
+                "seller": resultAuctionSell[0].toString(),
+                "startingPrice": resultAuctionSell[1].toString(),
+                "endingPrice": resultAuctionSell[2].toString(),
+                "duration": resultAuctionSell[3].toString(),
+                "startedAt": resultAuctionSell[4].toString(),
+                "currentPrice": resultSellPrice.toString(),
+            },
+            seed: {
+                "seller": resultAuctionSeed[0].toString(),
+                "startingPrice": resultAuctionSeed[1].toString(),
+                "endingPrice": resultAuctionSeed[2].toString(),
+                "duration": resultAuctionSeed[3].toString(),
+                "startedAt": resultAuctionSeed[4].toString(),
+                "currentPrice": resultSeedPrice.toString(),
+            }
+        };
     }
 
 
@@ -60,14 +99,15 @@ let sammy;
         });
 
         this.get('#/:tokenid', function(context) {
-            let token = {};
-            token["id"] = Number(this.params['tokenid']);
-            token["thumb"] = `https://s3-ap-northeast-1.amazonaws.com/crypton-live/thumbnails/${token.id}_512x586.png`;
-
-            $("#detailThumb img").attr("src", token.thumb);
-            getEntityFromTokenId(token.id).then(entity => {
-                Object.keys(entity).map((key) => {
-                    $(`#detailEntity #${key}`).html("<strong>" + key + "</strong><div>" + entity[key] + "</div>");
+            const tokenId = Number(this.params['tokenid']);
+            $("#detailThumb img").attr("src", `https://s3-ap-northeast-1.amazonaws.com/crypton-live/thumbnails/${tokenId}_512x586.png`);
+            getStateFromTokenId(tokenId).then(state => {
+                Object.keys(state).map(type => {
+                    Object.keys(state[type]).map((key) => {
+                        const value = state[type][key];
+                        const colSize = value.toString().length < 30 ? "col-lg-3 col-xl-6 card" : "col-lg-6 col-xl-12 card";
+                        $("#detailEntity .row").append(`<div class="${colSize}"><strong>${key}</strong><div>${value}</div></div>`);
+                    });
                 });
             });
         });
