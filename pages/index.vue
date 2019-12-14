a8ff8c78cd2d03f4ddbb8d1d119da97785c9cee5
planethouki <26595148+planethouki@users.noreply.github.com>
2019-12-14 11:26:05 +0900

    gtm

--- pages/index.vue
+++ pages/index.vue
@@ -23,8 +23,6 @@
             img.card-img-top(src="https://s3-ap-northeast-1.amazonaws.com/crypton-live/thumbnails/3_512x586.png")
             .card-body
               b-button(variant="primary" to="ton/3") See more
-      p
-        a(href="https://github.com/planethouki/crypton-explorer") Github
 </template>
 
 <script>
---
+++ pages/privacy.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container class="my-3">
+    <ul>
+      <li>このサイトでは、Cookieを使用しています。</li>
+      <li>このサイトでは、Google アナリティクスを使用しています。</li>
+      <li>
+        <a
+          href="https://www.google.com/intl/ja/policies/privacy/partners/"
+          target="_blank"
+        >
+          GOOGLE のサービスを使用するサイトやアプリから収集した情報の GOOGLE
+          による使用
+        </a>
+      </li>
+    </ul>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Privacy',
+  layout: 'top',
+  components: {},
+  computed: {}
+}
+</script>
