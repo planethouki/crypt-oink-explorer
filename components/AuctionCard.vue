1360f895e10269a6c6390694d42fb8d890fcadfb
planethouki <26595148+planethouki@users.noreply.github.com>
2020-10-03 16:23:03 +0900

    auction card all show style

--- components/AuctionCard.vue
+++ components/AuctionCard.vue
@@ -1,17 +1,19 @@
 <template lang="pug">
   .d-flex.flex-wrap
     template(v-for="ton in currentTons")
-      .card-wrap(
-        :key="ton.id"
-        v-if="asyncTonsCache[ton.id] && asyncTonsCache[ton.id][name].shown")
+      .card-wrap(:key="ton.id")
         .card
+          template(v-if="!asyncTonsCache[ton.id]")
+            .position-absolute(style="top:0;bottom:0;left:0;right:0;background:rgba(255,255,255,0.9);")
+              .position-absolute(style="top:50%;left:50%;transform:translate(-50%,-50%);")
+                b-spinner(label="Loading")
+          template(v-else-if="!asyncTonsCache[ton.id][name].shown")
+            .position-absolute(style="top:0;bottom:0;left:0;right:0;background:rgba(255,255,255,0.9);")
+              .position-absolute(style="top:50%;left:50%;transform:translate(-50%,-50%);")
+                span.d-inline-block.text-center.font-weight-bold.shadow Not in Auction
           .p-3.position-absolute
             nuxt-link(:to="`/ton/${ton.id}`") {{ ton.id }}
           img.w-100(:src="ton.imgSrc")
-    template(v-if="tonsLoaded")
-      span(v-if="nothingToShow") Nothing to show
-    template(v-else)
-      b-spinner(label="Spinning")
 
 </template>
 
@@ -33,21 +35,6 @@ export default {
     asyncTonsCache: {
       type: Object,
       required: true
-    }
-  },
-  computed: {
-    tonsLoaded() {
-      const tonsPending = this.currentTons.filter(
-        (ton) => !this.asyncTonsCache[ton.id]
-      )
-      return tonsPending.length === 0
-    },
-    nothingToShow() {
-      const tonsShown = this.currentTons
-        .filter((ton) => this.asyncTonsCache[ton.id])
-        .filter((ton) => this.asyncTonsCache[ton.id][this.name])
-        .filter((ton) => this.asyncTonsCache[ton.id][this.name].shown)
-      return tonsShown.length === 0
     }
   }
 }
