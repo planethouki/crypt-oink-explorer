d920a91a2e774b747f9094784d59ae4fb10f8118
planethouki <26595148+planethouki@users.noreply.github.com>
2023-03-10 14:32:57 +0900

    del eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,10 +35,7 @@ export default {
   /*
    ** Nuxt.js dev-modules
    */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
-  ],
+  buildModules: [],
   /*
    ** Nuxt.js modules
    */
